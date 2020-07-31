d6fdf313f9f941be258c123a935079a2c234b8ed
felix.xu <1092702994@qq.com>
2020-07-31 16:01:17 +0800

    a

--- main.go
+++ main.go
@@ -1,10 +1,64 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/base64"
 	"fmt"
+	"time"
 )
 
+
+
+var cipher = "密鑰"
+var h = md5.New()
+
+func cipherEncode(sourceText string) string {
+	h.Write([]byte(cipher))
+	cipherHash := fmt.Sprintf("%x", h.Sum(nil))
+	h.Reset()
+	inputData := []byte(sourceText)
+	loopCount := len(inputData)
+	outData := make([]byte,loopCount)
+	for i:= 0; i < loopCount ; i++ {
+		outData[i] = inputData[i] ^ cipherHash[i%32]
+	}
+	return fmt.Sprintf("%s", outData)
+}
+
+func encode(sourceText string) string {
+	h.Write([]byte(time.Now().Format("2006-01-02 15:04:05")))
+	noise := fmt.Sprintf("%x", h.Sum(nil))
+	h.Reset()
+	inputData := []byte(sourceText)
+	loopCount := len(inputData)
+	outData := make([]byte,loopCount*2)
+
+	for i, j := 0,0; i < loopCount ; i,j = i+1,j+1 {
+		outData[j] = noise[i%32]
+		j++
+		outData[j] = inputData[i] ^ noise[i%32]
+	}
+
+	return base64.StdEncoding.EncodeToString([]byte(cipherEncode(fmt.Sprintf("%s", outData))))
+}
+
+func decode(sourceText string) string {
+	buf, err := base64.StdEncoding.DecodeString(sourceText)
+	if err != nil {
+		fmt.Println("Decode(%q) failed: %v", sourceText, err)
+		return ""
+	}
+	inputData := []byte(cipherEncode(fmt.Sprintf("%s", buf)))
+	loopCount := len(inputData)
+	outData := make([]byte,loopCount)
+	for i, j := 0,0; i < loopCount ; i,j = i+2,j+1 {
+		outData[j] = inputData[i] ^ inputData[i+1]
+	}
+	return fmt.Sprintf("%s", outData)
+}
+
 func main() {
-	fmt.Println(123)
-	fmt.Println("我是测试")
+	s := encode("那家店好绿或都爱或到军大屁军大奥怕军都军奥奥奥奥奥奥奥奥奥奥奥奥奥多多多多多多多多多多多多多多多多多")
+	fmt.Println(s)
+	fmt.Println(decode(s))
 }
